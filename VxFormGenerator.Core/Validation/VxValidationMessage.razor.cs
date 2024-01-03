9c960132b6a7d46bef231bb7e2caf75d6109c793
Ivan Codarin <ivan.codarin@uniud.it>
2024-01-03 17:08:26 +0100

    fix #1: change invalid-feedback with text-danger to fix validation message

--- VxFormGenerator.Core/Validation/VxValidationMessage.razor.cs
+++ VxFormGenerator.Core/Validation/VxValidationMessage.razor.cs
@@ -4,6 +4,8 @@ namespace VxFormGenerator.Core.Validation
     public class VxValidationMessageComponent<TValue> : ValidationMessageBase<TValue>
     {
         public override string ValidClass { get; set; } = "valid-feedback";
-        public override string InValidClass { get; set; } = "invalid-feedback";
+        
+        //TODO: check if we can change invalide-feedback style on scss to avoid using text-danger and have correct validation message behavior #1
+        public override string InValidClass { get; set; } = "text-danger";
     }
 }
