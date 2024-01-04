438d30bd8cedbb2be359a8578dbdebc3799e4278
Ivan Codarin <ivan.codarin@uniud.it>
2024-01-04 17:49:52 +0100

    Introducing Invoice model to handle master-detail

---
+++ VxFormGeneratorDemoData/Invoice.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using VxFormGenerator.Core;
+using VxFormGenerator.Models;
+
+namespace VxFormGeneratorDemoData
+{
+    public class Invoice
+    {
+        [Display(Name = "Number")] 
+        public int Number { get; set; }
+
+        [Display(Name = "Date")] 
+        public DateTime Date { get; set; }
+
+        [Display(Name = "Customer name")] 
+        public string Customer { get; set; }
+
+    }
+
+}
